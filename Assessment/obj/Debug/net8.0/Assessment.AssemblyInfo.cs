9d784c11f950fa3388dd002b22dbe49c2de6d759
ThabisoHSN <gnyathela@gmail.com>
2024-10-09 19:11:04 +0200

    gitignore

--- Assessment/obj/Debug/net8.0/Assessment.AssemblyInfo.cs
+++ Assessment/obj/Debug/net8.0/Assessment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee9a79fd24722765110e344f5b48c7bfe417f65e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3c45f5313c9e2124a9719d3092a75b9513c94b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
