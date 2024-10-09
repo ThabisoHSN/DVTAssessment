43af8a9de4f4c3155c88ed764879215c35ebb36f
ThabisoHSN <gnyathela@gmail.com>
2024-10-09 19:23:23 +0200

    First Commit

--- Assessment/Program.cs
+++ Assessment/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Assessment.Classes;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var prompts = new SystemSetup();
+
+        
+
+        var operations = new Operation(prompts.BuildingFloors, prompts.Elevators, prompts.Capacity);
+
+        int option;
+
+        do
+        {
+            option = operations.RunOperations();
+            switch (option)
+            {
+                case 1:
+                    await operations.PerformStatusCheck();
+                    break;
+                case 2:
+                    await operations.PerformElevatorOperations();
+                    break;
+                default:
+                    break;
+            }
+        } while (option != 3);
+
+    }
+}
