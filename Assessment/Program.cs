5a5155aa7990152261542180a6c43f36b9289fb6
ThabisoHSN <gnyathela@gmail.com>
2024-10-09 19:29:59 +0200

    Code refactoring

--- Assessment/Program.cs
+++ Assessment/Program.cs
@@ -4,29 +4,41 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var prompts = new SystemSetup();
-
-        
-
-        var operations = new Operation(prompts.BuildingFloors, prompts.Elevators, prompts.Capacity);
-
-        int option;
-
-        do
+        try
         {
-            option = operations.RunOperations();
-            switch (option)
+            //Initialise and setup the building floor and how many elevators are in the building
+            var prompts = new SystemSetup();
+
+            var operations = new Operation(prompts.BuildingFloors, prompts.Elevators, prompts.Capacity);
+
+            int option;
+
+            do
             {
-                case 1:
-                    await operations.PerformStatusCheck();
-                    break;
-                case 2:
-                    await operations.PerformElevatorOperations();
-                    break;
-                default:
-                    break;
-            }
-        } while (option != 3);
+                //Determines which operations to run
+                option = operations.RunOperations();
+
+                switch (option)
+                {
+                    case 1:
+                        //Check for the current status of all the elevators
+                        await operations.PerformStatusCheck();
+                        break;
+                    case 2:
+                        //Performs the elevator operations
+                        await operations.PerformElevatorOperations();
+                        break;
+                    default:
+                        break;
+                }
+            } while (option != 3);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error at Main" + e.Message);
+        }
+
 
     }
 }
